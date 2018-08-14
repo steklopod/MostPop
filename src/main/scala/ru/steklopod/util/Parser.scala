70da188afba6c75ddac85e18fb019542644625aa
Dima Koltovich <dmitry.koltovich@stoloto.ru>
2018-08-14 14:11:41 +0300

    Parser test 0.1

--- src/main/scala/ru/steklopod/util/Parser.scala
+++ src/main/scala/ru/steklopod/util/Parser.scala
@@ -2,10 +2,22 @@ package ru.steklopod.util
 
 import java.net.URL
 
+import scala.io.Source
 import scala.util.Try
 
 object Parser {
 
   def parseURL(url: String): Try[URL] = Try(new URL(url))
+
+  def parseHttpUrl(url: String) = parseURL(url).filter(_.getProtocol == "http")
+
+  def getUrlContent(url: String): Try[Iterator[String]] = {
+    for {
+      url <- parseURL(url)
+      connection <- Try(url.openConnection())
+      is <- Try(connection.getInputStream())
+      source = Source.fromInputStream(is)
+    } yield source.getLines()
+  }
 
 }
--- src/test/scala/ru/steklopod/InitFunSuite.scala
+++ src/test/scala/ru/steklopod/InitFunSuite.scala
@@ -3,9 +3,12 @@ package ru.steklopod
 import org.scalatest.{FunSuite, Matchers}
 import ru.steklopod.util.Parser._
 
+import scala.util.{Failure, Success}
+
 class InitFunSuite extends FunSuite with Matchers {
 
   private[this] val sessionValue = "$2a$10$SjInPRoSm"
+  private[this] val url = "https://lenta.ru"
 
   test(" Test println() ") {
     println(sessionValue)
@@ -14,9 +17,14 @@ class InitFunSuite extends FunSuite with Matchers {
 
   test("Parser") {
 //    val keyWordObj = new KeyWord("смерть", 0, new URL("https://lenta.ru"))
+    parseURL(url).foreach(println)
+  }
 
-    parseURL("https://lenta.ru")
-
+  test("Get content from website"){
+    getUrlContent(url) match {
+      case Success(lines) => lines.foreach(println)
+      case Failure(ex) => println(s"Problem rendering URL content: ${ex.getMessage}")
+    }
 
   }
 
